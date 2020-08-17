2d935558739950d0ee21c1eee3f18af1404281db
camilo.rivera <camilo.rivera@globant.com>
2020-08-17 15:22:13 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>Hey i`m a Component</h2>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
